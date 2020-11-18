ee1087de912dfcb2406093f13aacf0659c51b506
Enoch-Kim <enochkim1220@gmail.com>
2020-11-18 17:32:48 +0900

    Add Model description * fix some field name * add gorm options * add json name

--- pkg/room/types.go
+++ pkg/room/types.go
@@ -7,38 +7,38 @@ import (
 
 type Room struct {
 	gorm.Model
-	GridID     uint             `json:"gridId" gorm:"index"`
-	Lct        string           `json:"location" gorm:"index"`
+	GridID     uint             `json:"gridId" gorm:"not null;index"`
+	Lct        string           `json:"location" gorm:"not null"`
 	EstType    uint             `json:"estateType" gorm:"not null;index"`
 	TrdType    string           `json:"tradeType" gorm:"not null;index"`
-	Title      string           `json:"title"`
-	Price      uint             `json:"price"`
-	Deposit    uint             `json:"deposit"`
+	Title      string           `json:"title" gorm:"not null"`
+	Price      uint             `json:"price" gorm:"not null"`
+	Deposit    uint             `json:"deposit" gorm:"not null"`
 	FloorStr   string           `json:"floorStr"`
-	RealSize   float64          `json:"realSize"`
-	RoughSize  float64          `json:"roughSize"`
-	Facilities util.ArrayStr    `json:"facilities"`
+	RealSize   float64          `json:"realSize" gorm:"not null"`
+	RoughSize  float64          `json:"roughSize" gorm:"not null"`
+	Facilities util.ArrayStr    `json:"facilities" gorm:"not null"`
 	ImgUrls    util.UrlArrayStr `json:"imgUrls"`
 }
 
 type Info struct {
 	gorm.Model
-	RoomID     uint             `json:"roomId" gorm:"index"`
-	GridID     uint             `json:"gridId" gorm:"index"`
-	Lct        string           `json:"location"`
-	EstType    uint             `json:"estateType"`
-	TrdType    string           `json:"tradeType"`
-	Title      string           `json:"title"`
-	Price      uint             `json:"price"`
-	Deposit    uint             `json:"deposit"`
+	RoomID     uint             `json:"roomId" gorm:"not null;index"`
+	GridID     uint             `json:"gridId" gorm:"not null;index"`
+	Lct        string           `json:"location" gorm:"not null"`
+	EstType    uint             `json:"estateType" gorm:"not null"`
+	TrdType    string           `json:"tradeType" gorm:"not null"`
+	Title      string           `json:"title" gorm:"not null"`
+	Price      uint             `json:"price" gorm:"not null"`
+	Deposit    uint             `json:"deposit" gorm:"not null"`
 	FloorStr   string           `json:"floorStr"`
-	RealSize   float64          `json:"realSize"`
-	RoughSize  float64          `json:"roughSize"`
-	Facilities util.ArrayStr    `json:"facilities"`
+	RealSize   float64          `json:"realSize" gorm:"not null"`
+	RoughSize  float64          `json:"roughSize" gorm:"not null"`
+	Facilities util.ArrayStr    `json:"facilities" gorm:"not null"`
 	ImgUrls    util.UrlArrayStr `json:"imgUrls"`
 	Addr       string           `json:"address"`
 	RoadAddr   string           `json:"roadAddress"`
-	Describe   string           `json:"describe"`
+	Descrip    string           `json:"describe"`
 	IsAnimal   bool             `json:"isAnimal"`
 	IsBalcony  bool             `json:"isBalcony"`
 	IsElevator bool             `json:"isElevator"`
